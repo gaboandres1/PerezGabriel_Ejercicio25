0cf4325e4da9fd148fc6875282c7cd76ac4d31d0
Default user <ga.perezg1@uniandes.edu.co>
2019-11-06 19:13:25 +0000

    Código del ejercicio 25. Comprobado.

--- clase25.cpp
+++ clase25.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-float **rellenar(float** M, int, int);
+float **rellenar(float **M, int, int);
 void imprimeCabezas(float *punteroPrimerElemento, int, int, int);
 void imprimeMatriz(float** M, int, int);
 
@@ -20,7 +20,7 @@ int main(void){
         float *p = M1[0];
         float *p2 = M1[1];
         
-        //imprimeCabezas(p, (int)(p2-p), n+2, n);
+        imprimeCabezas(p, (int)(p2-p), n+2, n);
         
     }
     else{
@@ -31,7 +31,7 @@ int main(void){
     return 0;
 }
 
-float **rellenar(int **M, int nFilas, int nColumnas){
+float **rellenar(float **M, int nFilas, int nColumnas){
     float **resp = new float*[nFilas];
     for(int i=0; i<nFilas; i++){
         resp[i] = new float[nColumnas];
@@ -42,20 +42,21 @@ float **rellenar(int **M, int nFilas, int nColumnas){
     imprimeMatriz(resp, nFilas, nColumnas);
     return resp;
 }
-/*
+
 void imprimeCabezas(float *punteroPrimerElemento, int deltaAddr, int nFilas, int nColumnas){
     float suma = 0.0;
     for(int i=0; i<nFilas; i++){
         if(i == 0){
+            std::cout<<"La primera fila es [";
             for(int j=0; j<nColumnas; j++){
-                std::cout<<"La primera fila es ["<<*(punteroPrimerElemento+j);
+                std::cout<<*(punteroPrimerElemento+j)<<" ";
             }
             std::cout<<"]"<<std::endl;
         }
         suma += *(punteroPrimerElemento+i*deltaAddr);
     }
     std::cout << "Los elementos de la primera columna suman "<< suma << std::endl;
-}*/
+}
 
 void imprimeMatriz(float **M, int nFilas, int nColumnas){
     for(int i=0; i<nFilas; i++){
